95219ab77228c09fe57950791db9a1cdc1e96e1a
A-Gully <agallul@yahoo.com>
2022-10-10 11:54:36 +0100

    Done

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,20 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    mySet = set(items)
+    print(mySet)
+    for item in mySet:
+        count = 0
+        print(item)
+        for x in items:
+            if f'{x}' == f'{item}':
+                count += 1
+        frequencies[f'{item}'] = count
+        print(frequencies)
     return frequencies
+
+
+frequencies(['0', 4, 4, '4', 'd', 'd', 'e', 0, 'a', 'd', '4'])
